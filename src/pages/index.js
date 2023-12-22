618f66af6fc991d1c192fe3f94a89665747233f0
vincent-backend <yutaka@inc-acv.com>
2023-12-22 19:58:55 +0700

    GIF Animation

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,7 @@ import faqs from "@/content/faqs.json";
 import { useEffect, useState } from "react";
 import { getDataFromContent } from "@/lib/contentParser";
 import useTranslation from "@/hooks/useTranslation";
+import { markdownify } from "@/lib/utils/textConverter";
 
 export default function Home({data}) {
   const { locale } = useTranslation();
@@ -33,7 +34,9 @@ export default function Home({data}) {
   return (
     <Base>
       {/*background*/}
-      <div className="absolute w-full h-[635px] md:h-full bg-cover bg-center bg-[url('/images/home/bg_top_pic.png')] md:bg-[url('/images/home/md/bg_top_pic.png')] -z-10" />
+      <div className="absolute w-[750px] h-[1267px] md:w-[1921px] md:h-[1187px] top-0 left-0 bg-[url('/images/home/bg_top_pic.png')] md:bg-[url('/images/home/md/bg_top_pic.png')] -z-10" />
+      {/* top graph */}
+      <div className="absolute w-[750px] h-[750px] top-[150px] left-[1020px] bg-[url('/images/home/top_graph.gif')] md:bg-[url('/images/home/md/top_graph.gif')] -z-10" />
       <div className="absolute w-[50%] md:w-[358px] h-[670px] bg-contain bg-no-repeat bg-[url('/images/home/top_left_line.svg')] -z-10" />
       <div className="absolute w-[661px] md:w-[1100px] h-[995px] md:h-[1658px] top-[700px] md:top-[389px] right-0 bg-[url('/images/home/bg_right_light.svg')] md:bg-[url('/images/home/md/bg_right_light.svg')] opacity-70 -z-10" />
       <div className="absolute w-[306px] h-[804px] md:w-[706px] top-[1070px] md:top-[1611px] left-0 bg-[url('/images/home/line.png')] md:bg-[url('/images/home/md/line.png')] -z-10" />
@@ -42,13 +45,12 @@ export default function Home({data}) {
       <div className="container pt-[110px] md:pt-[254px]">
         <section className="animate">
           {/* Banner */}
-          <div className="flex flex-col lg:flex-row">
-            <div className="flex flex-col justify-start w-full">
+          <div className="flex flex-col justify-start md:max-w-[670px]">
               <h2 className="whitespace-nowrap">{banner.title}</h2>
               <h1 className="text-cred">$AVAV</h1>
-              <p className="mt-[20px] md:mt-[136px] text-white leading-6">
-               {banner.content}
-              </p>
+              <div className="mt-[20px] md:mt-[136px]">
+                {markdownify(banner.content, "h6", "text-white leading-6")}
+              </div>
               <Link
                 href="/"
                 className="bg-[url('/images/home/banner_btn_bg.svg')] w-[200px] h-[50px] text-center text-white leading-[50px] mt-[20px] md:mt-[30px]"
@@ -56,20 +58,6 @@ export default function Home({data}) {
                 {banner.btn_con}
               </Link>
             </div>
-            <div className="max-w-[670px] w-full relative">
-              <Image
-                alt="banner-img"
-                src="/images/home/top_graph.png"
-                resizemode={"contain"}
-                width={650}
-                height={758}
-              />
-              <Link
-                href="/"
-                className="absolute md:hidden right-[2%] sm:right-[3%] bottom-[30%] w-[50px] h-[50px] bg-contain bg-no-repeat bg-[url('/images/home/arrow.svg')]"
-              />
-            </div>
-          </div>
           {/* Site Link */}
           <div className="flex flex-wrap justify-center lg:justify-start max-w-[880px] gap-4 mt-[24px] md:mt-[60px]">
             <Link href="/" className="site-link">
