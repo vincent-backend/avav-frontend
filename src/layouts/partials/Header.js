a775a0f66b676ef8aeb9ad04b07be637b5d6e47e
vincent-backend <yutaka@inc-acv.com>
2023-12-24 12:37:48 +0700

    Language fix

--- src/layouts/partials/Header.js
+++ src/layouts/partials/Header.js
@@ -102,7 +102,7 @@ const Header = () => {
               ))}
               <li className="nav-item nav-dropdown group relative mt-[50%] md:mt-0">
                 <span className="nav-link inline-flex items-center">
-                  {locale == "jp" ? "日本語" : locale == "cn" ? "中文" : locale == "zh" ? "中文（台灣）" : "EN" }
+                  {locale == "jp" ? "日本語" : locale == "cn" ? "中文" : locale == "zh" ? "中文（繁体）" : "EN" }
                   <svg className="h-4 w-4 fill-current ml-1" viewBox="0 0 20 20">
                     <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
                   </svg>
