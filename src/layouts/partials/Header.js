cd82714e834f6cbe555bbc314bbd5646dd193af4
vincent-backend <yutaka@inc-acv.com>
2023-12-25 13:52:08 +0700

    navbar

--- src/layouts/partials/Header.js
+++ src/layouts/partials/Header.js
@@ -113,7 +113,7 @@ const Header = () => {
                 ) : (
                   <li className="nav-item">
                     <Link
-                      href={menu.url}
+                      href={menu.url} target="_blank"
                       className={`nav-link block ${
                         asPath === menu.url && "active"
                       }`}
