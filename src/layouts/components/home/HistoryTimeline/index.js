8a23419a54d7b2b7414ef1b4d695f47b9312e719
vincent-backend <yutaka@inc-acv.com>
2023-12-27 15:43:46 +0700

    timeline font size

--- src/layouts/components/home/HistoryTimeline/index.js
+++ src/layouts/components/home/HistoryTimeline/index.js
@@ -28,11 +28,9 @@ function TimeLineBloc({ data, direction }) {
   if (direction == "left") {
     return (
       <div className="flex flex-col md:flex-row-reverse justify-between items-center w-full">
-        <p className="w-full pl-[40px] text-cred font-primary font-bold text-left text-[18px] md:text-[30px]">
-          {data.date}
-        </p>
+        <p className="w-full pl-[40px] text-cred font-primary font-bold text-left text-[18px] md:text-[26px]">{data.date}</p>
         <div className="relative w-full">
-          <p className="text-[16px] text-white font-primary text-left md:text-right bg-[#1A1C1F] rounded-md p-5 ml-[40px] md:ml-0 md:mr-[28px]">{data.content}</p>
+          <p className="text-[14px] md:text-[14px] text-white font-primary text-left md:text-right bg-[#1A1C1F] rounded-md p-3 md:p-5 ml-[40px] md:ml-0 md:mr-[28px]">{data.content}</p>
           <div className="absolute w-[11px] h-[20px] left-[31px] md:left-auto md:right-[19px] top-[calc(50%-10px)] bg-no-repeat bg-[url('/images/home/about_card_ic_arrow_left.svg')] md:bg-[url('/images/home/about_card_ic_arrow_right.svg')] " />
           <div className="bg-cred w-[14px] h-[14px] rounded-full absolute left-[13px] right-auto md:left-auto md:right-[-17px] top-[calc(50%-7px)]" />
         </div>
@@ -41,11 +39,9 @@ function TimeLineBloc({ data, direction }) {
   } else {
     return (
       <div className="flex flex-col md:flex-row justify-between items-center w-full">
-        <p className="w-full pl-[40px] md:pl-0 md:pr-[45px] text-cred font-primary font-bold text-left md:text-right text-[18px] md:text-[30px]">
-          {data.date}
-        </p>
+        <p className="w-full pl-[40px] md:pl-0 md:pr-[45px] text-cred font-primary font-bold text-left md:text-right text-[18px] md:text-[26px]">{data.date}</p>
         <div className="relative w-full">
-          <p className="text-[16px] text-white font-primary text-left bg-[#1A1C1F] rounded-md p-5 ml-[40px] md:ml-[25px]">{data.content}</p>
+          <p className="text-[14px] md:text-[14px] text-white font-primary text-left bg-[#1A1C1F] rounded-md p-3 md:p-5 ml-[40px] md:ml-[25px]">{data.content}</p>
           <div className="absolute w-[11px] h-[20px] left-[31px] md:left-[15px] top-[calc(50%-10px)] bg-no-repeat bg-[url('/images/home/about_card_ic_arrow_left.svg')] " />
           <div className="bg-cred w-[14px] h-[14px] rounded-full absolute left-[13px] right-auto md:left-[-18px] top-[calc(50%-7px)]" />
         </div>
