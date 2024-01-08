7d5f0109e88c09d38ce510f0b09bdde1707c0c78
vincent-backend <yutaka@inc-acv.com>
2024-01-08 11:37:54 +0700

    Small fix

--- src/layouts/components/art/ArtCategoryElement.js
+++ src/layouts/components/art/ArtCategoryElement.js
@@ -2,8 +2,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import { useEffect, useState } from "react";
-import Zoom from "react-medium-image-zoom";
-
+import clsx from "clsx";
 import config from "@/config/config.json";
 import useTranslation from "@/hooks/useTranslation";
 
@@ -47,9 +46,11 @@ function ArtCategoryElement({category, votes}) {
             <p className="text-white font-primary text-[14px] md:text-[18px] group-hover:text-cred group-active:text-cred break-all">{category.name[locale]}</p>
             <div className="absolute w-full md:w-4/5 h-[38px] bottom-0 md:bottom-6 bg-[#FD2C2F] text-white cursor-pointer md:opacity-0 md:group-hover:opacity-100 transition-opacity duration-100 ease-linear"
                     onClick={()=>addVote()}>
-                <div className="flex flex-row w-full h-full items-center justify-center gap-1">
-                    <Image alt="thumb" src="/images/art/pic_hover_ic_vote.svg" width={16} height={16} />
-                    <p className="font-primary text-[14px] text-white">{`Vote (${vote})`}</p>
+                <div className="w-full h-full relative">
+                    <p className="absolute w-full top-0 font-primary text-[14px] leading-[38px] text-white text-center">
+                        <Image alt="thumb" src="/images/art/pic_hover_ic_vote.svg" width={16} height={16} className={clsx("inline-block me-1")} />
+                        {`Vote (${vote})`}
+                    </p>
                 </div>
             </div>
         </div>
